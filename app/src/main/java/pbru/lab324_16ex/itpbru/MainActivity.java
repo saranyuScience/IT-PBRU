541dad9806903b15070ecdc92a536c1752b372a1
saran <My-spoy14@hotmail.co.th>
2016-06-06 14:18:00 +0700

    Start app

---
+++ app/src/main/java/pbru/lab324_16ex/itpbru/MainActivity.java
@@ -0,0 +1,13 @@
+package pbru.lab324_16ex.itpbru;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
